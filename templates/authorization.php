ff311007447a08630896fad0ed95d30a6fb9b465
Artyom <mortem@ngs.ru>
2022-07-27 15:48:30 +0700

    Переименование файлов, содержащих сообщения об успешной и неуспешной авторизации

--- templates/authorization.php
+++ templates/authorization.php
@@ -47,11 +47,11 @@
             <span>Ваш e-mail: <b><?= $convertLogin ?></b><br/><br/></span>
             <span>Ваш статус: <b><?= $statusUser ?></b><br/><br/></span>
             <?php if (isset($flLogInSuccess) && $flLogInSuccess): ?>
-                <span id="success"><?php include $_SERVER['DOCUMENT_ROOT'] . '/include/success.php'; ?><br/><br/></span>
+                <span id="success"><?php include $_SERVER['DOCUMENT_ROOT'] . '/include/msg_auth_success.php'; ?><br/><br/></span>
             <?php endif ?>
             <input class="button" type="submit" name="log_out" value="Выйти">
         <?php elseif (isset($isShowErrorLogIn) && $isShowErrorLogIn): ?>
-            <span id="error"><?php include $_SERVER['DOCUMENT_ROOT'] . '/include/error.php'; ?><br/><br/></span>
+            <span id="error"><?php include $_SERVER['DOCUMENT_ROOT'] . '/include/msg_auth_error.php'; ?><br/><br/></span>
             <input type="email" name="login" class="custom-form__input" placeholder="Ваш e-mail" value="<?= htmlspecialchars($loginError) ?>" required="">
             <input type="password" name="password" class="custom-form__input" placeholder="Ваш пароль" value="<?= htmlspecialchars($passwordError) ?>" required="">
             <button class="button" type="submit">Войти в личный кабинет</button>
